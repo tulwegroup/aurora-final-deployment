0505eb2e2fffd7e16b5ec9539b56d3f958880055
achintir <achintir77@gmail.com>
2026-03-26 10:24:48 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
+import { AuthProvider, useAuth } from '@/lib/AuthContext';
+import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import ScanHistory from './pages/ScanHistory';
